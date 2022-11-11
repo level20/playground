abc2b679c5e3ca1b6377441fef50437501d3f6bc
Feras Deiratany <fdeiratany@level20.com>
2022-11-11 15:59:41 -0800

    fix side bar

--- client/components/layout/default-layout.tsx
+++ client/components/layout/default-layout.tsx
@@ -1,35 +1,54 @@
 import { Layout } from "antd";
+import { Footer } from "antd/lib/layout/layout";
 const { Header, Sider, Content } = Layout;
 import Title from "antd/lib/typography/Title";
-
 import SideBar from "./side-bar";
 
 const DefaultLayout = ({ hideHeader, headerTitle, children }: any) => {
   return (
-    <Layout hasSider>
+    <Layout>
       <Sider
-        style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          bottom: 0,
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
         }}
       >
+        <div className="logo">
+          <Title level={2} style={{ color: "#fff" }}>
+            Playground
+          </Title>
+        </div>
         <SideBar />
       </Sider>
-      <Layout style={{ marginLeft: 200 }}>
+      <Layout>
         {!hideHeader && (
-          <Header
-            style={{ backgroundColor: "aquamarine", textAlign: "center" }}
-          >
-            <Title>{headerTitle}</Title>
+          <Header className="site-layout-sub-header-background">
+            <Title level={1} style={{ color: "#fff", margin: '8px' }}>
+              {headerTitle}
+            </Title>
           </Header>
         )}
-        <Content style={{ overflow: "initial", height: "100vh" }}>
-          {children}
+        <Content
+          style={{
+            margin: "24px 16px 0",
+            overflow: "initial",
+            height: "100vh",
+          }}
+        >
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360, height: "100vh" }}
+          >
+            {children}
+          </div>
         </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Level20 Playground ©2022 Created by Level20
+        </Footer>
       </Layout>
     </Layout>
   );
--- client/components/layout/side-bar.tsx
+++ client/components/layout/side-bar.tsx
@@ -1,14 +1,18 @@
 import { useRouter } from "next/router";
-import { Menu, Typography, Divider } from "antd";
+import { Menu, Typography, Divider, Button } from "antd";
 const { Title } = Typography;
-import { CodeOutlined, SolutionOutlined, DesktopOutlined } from "@ant-design/icons";
-import React from "react";
+import {
+  CodeOutlined,
+  SolutionOutlined,
+  DesktopOutlined,
+} from "@ant-design/icons";
+import React, { useState } from "react";
 import { Routes } from "../../enums/routes.enum";
 
 enum MenuItem {
   Creations = "Creations",
   Blog = "Blog",
-  Studio = "Studio"
+  Studio = "Studio",
 }
 
 const items = [
@@ -25,11 +29,11 @@ const items = [
     icon: React.createElement(SolutionOutlined),
   },
   {
-    key: MenuItem.Studio, 
-    label: MenuItem.Studio, 
+    key: MenuItem.Studio,
+    label: MenuItem.Studio,
     route: Routes.Studio,
-    icon: React.createElement(DesktopOutlined)
-  }
+    icon: React.createElement(DesktopOutlined),
+  },
 ];
 
 const SideBar = () => {
@@ -40,20 +44,10 @@ const SideBar = () => {
     router.push(item?.props?.route);
   };
 
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <>
-      <Title
-        level={3}
-        style={{
-          color: "white",
-          marginTop: "8px",
-          marginBottom: "8px",
-          marginLeft: "16px",
-        }}
-      >
-        Playground
-      </Title>
-      <Divider style={{ borderColor: "aquamarine" }} />
       <Menu theme="dark" mode="inline" items={items} onClick={onClick}></Menu>
     </>
   );
