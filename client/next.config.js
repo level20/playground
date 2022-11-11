2d383baa9f9ed3e2026965953042810720b91487
Feras Deiratany <fdeiratany@level20.com>
2022-11-10 20:10:21 -0800

    add sanity to next

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_TOKEN: process.env.SANITY_TOKEN,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
